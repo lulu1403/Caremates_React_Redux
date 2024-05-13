36baab576b6857b87f8d8078a52f36cb8b362f57
lulu1403 <kulumfatma14@gmail.com>
2024-05-13 17:49:58 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import CounterComponent from "./CounterComponent";
+import UserComponent from "./UserComponent";
+
+function App() {
+  return (
+    <>
+    <CounterComponent />
+    <br />
+    <UserComponent />
+    </>
+  );
+}
+
+export default App
